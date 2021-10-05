05266aa64f93d12240fd83d0bacf8177c8f5e9e4
ramonvax <ramonvax@gmail.com>
2021-10-05 17:46:06 -0300

    creating the initial interface

--- AppMachine.java
+++ AppMachine.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class AppMachine {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        MoneyMachine cashRegister = new MoneyMachine();
+        final int LINESIZE = 80;
+        String command;
+
+        while (true) {
+            System.out.println("Whats do you like?");
+            command = input.nextLine();
+            if (command.equals("off")) {
+                for (int i = 0; i < LINESIZE; i++) {
+                    System.out.print("-");
+                }
+                System.out.println("\nSystem shutdown");
+                break;
+            } else if (command.equals("report")) {
+                System.out.println("alfa test");
+            } else if (command.equals("espresso") || command.equals("latte") || command.equals("cappuccino")) {
+                System.out.println("beta test");
+            } else {
+                System.out.println("comand not understood");
+            }
+            
+        }
+
+    }
+
 }
--- MoneyMachine.java
+++ MoneyMachine.java
@@ -1,3 +1,8 @@
 public class MoneyMachine {
-    
+    private final String CURRENCY = "R$";
+    private double profit = 0, moneyReceived = 0;
+
+    public void report() {
+        System.out.println("Money: " + CURRENCY + profit);
+    }
 }
